b5d48316c9efc8e7c4467ce987322fce5f0f87da
basant <basantamin@outlook.sa>
2024-12-06 16:08:46 +0300

    Initial Commit

---
+++ MyHappyTime/MyHappyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHappyTimeApp.swift
+//  MyHappyTime
+//
+//  Created by basant amin bakir on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyHappyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
